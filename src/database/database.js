09208d8a3edabbdb0b519b4ad2c81c6ef1813ae7
diego <diego@DESKTOP-QNU4K35>
2022-09-21 03:33:25 -0500

    add2

--- src/database/database.js
+++ src/database/database.js
@@ -1,11 +1,19 @@
 import Sequelize from 'sequelize'
+import path from 'path';
+import { fileURLToPath } from 'url';
+import fs from 'fs'
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 export const sequelize = new Sequelize(
     'pry20220174',
-    'root',
-    'password',
+    '7nk004wtj89n31e3lo48',
+    'pscale_pw_quoMbwaq0hPMtpZLDdUoj29iEuUgxJJwXFuyul48ZPW',
     {
-    host : '127.0.0.1',
+    host : 'us-east.connect.psdb.cloud',
     dialect : 'mysql',
+    ssl: {
+        cert: fs.readFileSync(__dirname + '/etc/pki/tls/certs/ca-bundle.crt'),
+    }
     }
 );
