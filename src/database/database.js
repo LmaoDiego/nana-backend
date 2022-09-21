a5cf103779fb63c354d691eb42c20196dd74c58b
diego <diego@DESKTOP-QNU4K35>
2022-09-21 13:29:32 -0500

    final

--- src/database/database.js
+++ src/database/database.js
@@ -3,17 +3,21 @@ import path from 'path';
 import { fileURLToPath } from 'url';
 import fs from 'fs'
 
+DB_HOST=localhost us-east.connect.psdb.cloud
+DB_NAME=pry20220174
+DB_USER=root m23at3xfvbbw0zuetaa6
+DB_PASSWORD=password pscale_pw_SwejjM1B06porzLMpmzRcLFf1RcyMYocMNK1kRVjPlq
+
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 export const sequelize = new Sequelize(
     'pry20220174',
-    '7nk004wtj89n31e3lo48',
-    'pscale_pw_quoMbwaq0hPMtpZLDdUoj29iEuUgxJJwXFuyul48ZPW',
+    'm23at3xfvbbw0zuetaa6',
+    'pscale_pw_SwejjM1B06porzLMpmzRcLFf1RcyMYocMNK1kRVjPlq',
     {
     host : 'us-east.connect.psdb.cloud',
     dialect : 'mysql',
-    ssl: {
-        cert: fs.readFileSync(__dirname + '/etc/pki/tls/certs/ca-bundle.crt'),
-    }
+    ssl={"rejectUnauthorized":true}
     }
 );
